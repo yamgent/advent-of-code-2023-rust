7b20ea760968731a3c03d0111cdda0cabb4e48b7
Maurice Wangleng Tan <tanwangleng@outlook.com>
2023-12-02 13:45:51 +0800

    Solve day 02 part 2

--- src/bin/day02/main.rs
+++ src/bin/day02/main.rs
@@ -1,21 +1,26 @@
 const ACTUAL_INPUT: &str = include_str!("../../../actual_inputs/2023/02/input.txt");
 
-fn p1(input: &str) -> String {
-    const LIMIT: (u32, u32, u32) = (12, 13, 14);
+type GameRecord = (u32, u32, u32);
 
+struct Game {
+    id: usize,
+    records: Vec<GameRecord>,
+}
+
+fn parse_input(input: &str) -> Vec<Game> {
     input
         .trim()
         .lines()
         .enumerate()
-        .flat_map(|(id, line)| {
+        .map(|(id, line)| {
             let id = id + 1;
             let (_, records) = line.split_once(':').unwrap();
 
-            let exceed = records
+            let records = records
                 .trim()
                 .split(';')
-                .find(|record| {
-                    let record = record.trim().split(',').fold((0, 0, 0), |acc, current| {
+                .map(|record| {
+                    record.trim().split(',').fold((0, 0, 0), |acc, current| {
                         let (count, color) = current.trim().split_once(' ').unwrap();
                         let count = count.parse::<u32>().unwrap();
 
@@ -25,25 +30,45 @@ fn p1(input: &str) -> String {
                             "blue" => (acc.0, acc.1, count),
                             _ => unreachable!(),
                         }
-                    });
-
-                    record.0 > LIMIT.0 || record.1 > LIMIT.1 || record.2 > LIMIT.2
+                    })
                 })
-                .is_some();
+                .collect::<Vec<_>>();
 
-            if exceed {
-                None
-            } else {
-                Some(id)
-            }
+            Game { id, records }
         })
+        .collect()
+}
+
+fn p1(input: &str) -> String {
+    const LIMIT: (u32, u32, u32) = (12, 13, 14);
+
+    parse_input(input)
+        .into_iter()
+        .filter(|game| {
+            game.records
+                .iter()
+                .all(|record| record.0 <= LIMIT.0 && record.1 <= LIMIT.1 && record.2 <= LIMIT.2)
+        })
+        .map(|game| game.id)
         .sum::<usize>()
         .to_string()
 }
 
 fn p2(input: &str) -> String {
-    let _input = input.trim();
-    "".to_string()
+    parse_input(input)
+        .into_iter()
+        .map(|game| {
+            let max_each = game.records.iter().fold((0, 0, 0), |acc, current| {
+                (
+                    acc.0.max(current.0),
+                    acc.1.max(current.1),
+                    acc.2.max(current.2),
+                )
+            });
+            max_each.0 * max_each.1 * max_each.2
+        })
+        .sum::<u32>()
+        .to_string()
 }
 
 fn main() {
@@ -55,18 +80,17 @@ fn main() {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_p1_sample() {
-        assert_eq!(
-            p1(r"
+    const SAMPLE_INPUT: &'static str = r"
 Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-"),
-            "8"
-        );
+";
+
+    #[test]
+    fn test_p1_sample() {
+        assert_eq!(p1(SAMPLE_INPUT), "8");
     }
 
     #[test]
@@ -76,12 +100,11 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 
     #[test]
     fn test_p2_sample() {
-        assert_eq!(p2(""), "");
+        assert_eq!(p2(SAMPLE_INPUT), "2286");
     }
 
     #[test]
-    #[ignore = "not yet implemented"]
     fn test_p2_actual() {
-        assert_eq!(p2(ACTUAL_INPUT), "");
+        assert_eq!(p2(ACTUAL_INPUT), "63700");
     }
 }
