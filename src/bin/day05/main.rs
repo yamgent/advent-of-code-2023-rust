28f8957128e54943c32cc0f447ad3f0beb25df0a
Maurice Wangleng Tan <tanwangleng@outlook.com>
2023-12-06 00:01:07 +0800

    Solve day 05 part 2

--- src/bin/day05/main.rs
+++ src/bin/day05/main.rs
@@ -1,10 +1,63 @@
 const ACTUAL_INPUT: &str = include_str!("../../../actual_inputs/2023/05/input.txt");
 
+#[derive(Debug, Clone)]
+struct Range {
+    start: u64,
+    len: u64,
+}
+
+struct Offset(u64);
+
+impl Range {
+    fn offset(&self, num: u64) -> Option<Offset> {
+        if num >= self.start && num < self.start + self.len {
+            Some(Offset(num - self.start))
+        } else {
+            None
+        }
+    }
+
+    fn num_from_offset(&self, offset: Offset) -> Option<u64> {
+        if offset.0 < self.len {
+            Some(self.start + offset.0)
+        } else {
+            None
+        }
+    }
+
+    fn interval_inclusive(&self) -> (u64, u64) {
+        (self.start, self.start + self.len - 1)
+    }
+
+    fn overlap(&self, other: &Range) -> Option<Range> {
+        let self_interval = self.interval_inclusive();
+        let other_interval = other.interval_inclusive();
+
+        if self_interval.0 > other_interval.1 || self_interval.1 < other_interval.0 {
+            None
+        } else {
+            let overlap_interval = (
+                self_interval.0.max(other_interval.0),
+                self_interval.1.min(other_interval.1),
+            );
+            Some(Range {
+                start: overlap_interval.0,
+                len: overlap_interval.1 - overlap_interval.0 + 1,
+            })
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Mapper {
-    dst_range_start: u64,
-    src_range_start: u64,
-    range_len: u64,
+    dst: Range,
+    src: Range,
+}
+
+#[derive(Debug)]
+struct ConvertResult {
+    success: Option<Range>,
+    failures: Vec<Range>,
 }
 
 impl Mapper {
@@ -16,17 +69,51 @@ impl Mapper {
         let range_len = iter.next().unwrap();
 
         Self {
-            dst_range_start,
-            src_range_start,
-            range_len,
+            dst: Range {
+                start: dst_range_start,
+                len: range_len,
+            },
+            src: Range {
+                start: src_range_start,
+                len: range_len,
+            },
         }
     }
 
-    fn convert(&self, src_num: u64) -> Option<u64> {
-        if src_num >= self.src_range_start && src_num < self.src_range_start + self.range_len {
-            Some(src_num - self.src_range_start + self.dst_range_start)
-        } else {
-            None
+    fn convert(&self, input: &Range) -> ConvertResult {
+        match input.overlap(&self.src) {
+            None => ConvertResult {
+                success: None,
+                failures: vec![input.clone()],
+            },
+            Some(overlap) => {
+                let mut failures = vec![];
+
+                if input.start < overlap.start {
+                    failures.push(Range {
+                        start: input.start,
+                        len: overlap.start - input.start,
+                    });
+                }
+
+                if input.start + input.len > overlap.start + overlap.len {
+                    failures.push(Range {
+                        start: overlap.start + overlap.len,
+                        len: input.start + input.len - (overlap.start + overlap.len),
+                    });
+                }
+
+                ConvertResult {
+                    success: Some(Range {
+                        start: self
+                            .dst
+                            .num_from_offset(self.src.offset(overlap.start).unwrap())
+                            .unwrap(),
+                        len: overlap.len,
+                    }),
+                    failures,
+                }
+            }
         }
     }
 }
@@ -63,18 +150,68 @@ impl Input {
     }
 }
 
+fn handle_seed_range(seed_range: &Range, maps: &Vec<Vec<Mapper>>) -> u64 {
+    maps.iter()
+        .fold(vec![seed_range.clone()], |ranges, current_level| {
+            ranges
+                .iter()
+                .flat_map(|range| {
+                    struct RangeCollection {
+                        successes: Vec<Range>,
+                        failures: Vec<Range>,
+                    }
+
+                    let final_ans = current_level.iter().fold(
+                        RangeCollection {
+                            successes: vec![],
+                            failures: vec![range.clone()],
+                        },
+                        |acc, map| {
+                            let mut new_acc = RangeCollection {
+                                successes: acc.successes,
+                                failures: vec![],
+                            };
+
+                            acc.failures.iter().for_each(|failure| {
+                                let result = map.convert(failure);
+                                if let Some(success) = result.success {
+                                    new_acc.successes.push(success);
+                                }
+                                new_acc.failures.extend(result.failures);
+                            });
+
+                            new_acc
+                        },
+                    );
+
+                    final_ans
+                        .successes
+                        .iter()
+                        .cloned()
+                        .chain(final_ans.failures.iter().cloned())
+                        .collect::<Vec<_>>()
+                })
+                .collect()
+        })
+        .into_iter()
+        .map(|range| range.start)
+        .min()
+        .unwrap()
+}
+
 fn p1(input: &str) -> String {
     let input = Input::parse_input(input);
     input
         .seeds
         .into_iter()
         .map(|seed| {
-            input.maps.iter().fold(seed, |src, current| {
-                current
-                    .iter()
-                    .find_map(|mapper| mapper.convert(src))
-                    .unwrap_or(src)
-            })
+            handle_seed_range(
+                &Range {
+                    start: seed,
+                    len: 1,
+                },
+                &input.maps,
+            )
         })
         .min()
         .unwrap()
@@ -82,8 +219,22 @@ fn p1(input: &str) -> String {
 }
 
 fn p2(input: &str) -> String {
-    let _input = input.trim();
-    "".to_string()
+    let input = Input::parse_input(input);
+    input
+        .seeds
+        .chunks(2)
+        .map(|seed_range| {
+            handle_seed_range(
+                &Range {
+                    start: seed_range[0],
+                    len: seed_range[1],
+                },
+                &input.maps,
+            )
+        })
+        .min()
+        .unwrap()
+        .to_string()
 }
 
 fn main() {
@@ -143,12 +294,11 @@ humidity-to-location map:
 
     #[test]
     fn test_p2_sample() {
-        assert_eq!(p2(SAMPLE_INPUT), "");
+        assert_eq!(p2(SAMPLE_INPUT), "46");
     }
 
     #[test]
-    #[ignore = "not yet implemented"]
     fn test_p2_actual() {
-        assert_eq!(p2(ACTUAL_INPUT), "");
+        assert_eq!(p2(ACTUAL_INPUT), "148041808");
     }
 }
