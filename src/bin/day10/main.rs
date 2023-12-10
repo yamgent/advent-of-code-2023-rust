f06bcdaf3fb467608c8c8b7e27f5a9a2c37eac19
Maurice Wangleng Tan <tanwangleng@outlook.com>
2023-12-10 16:35:51 +0800

    Day 10: Add part 2 sample inputs

--- src/bin/day10/main.rs
+++ src/bin/day10/main.rs
@@ -245,9 +245,62 @@ LJ.LJ
         assert_eq!(p1(ACTUAL_INPUT), "7097");
     }
 
+    const SAMPLE_INPUT_P2: [&str; 4] = [
+        r"
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+",
+        r"
+..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+..........
+",
+        r"
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+",
+        r"
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+",
+    ];
+
     #[test]
+    #[ignore = "not yet implemented"]
     fn test_p2_sample() {
-        assert_eq!(p2(SAMPLE_INPUT_P1[0]), "");
+        assert_eq!(p2(SAMPLE_INPUT_P2[0]), "4");
+        assert_eq!(p2(SAMPLE_INPUT_P2[1]), "4");
+        assert_eq!(p2(SAMPLE_INPUT_P2[2]), "8");
+        assert_eq!(p2(SAMPLE_INPUT_P2[3]), "8");
     }
 
     #[test]
