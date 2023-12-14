7855fed95f8fe243ddd1714105c3b31bb36e84cb
Maurice Wangleng Tan <tanwangleng@outlook.com>
2023-12-14 22:29:05 +0800

    Solve day 14 part 2

--- src/bin/day14/main.rs
+++ src/bin/day14/main.rs
@@ -49,6 +49,88 @@ fn roll_rocks_north(world: &mut World) {
     });
 }
 
+fn roll_rocks_west(world: &mut World) {
+    let mut leftest = std::iter::repeat(-1).take(world.len()).collect::<Vec<_>>();
+
+    (0..world[0].len()).for_each(|x| {
+        (0..world.len()).for_each(|y| match world[y][x] {
+            Space::RoundRock => {
+                leftest[y] += 1;
+                let final_x = leftest[y] as usize;
+                world[y][x] = Space::Empty;
+                world[y][final_x] = Space::RoundRock;
+            }
+            Space::CubeRock => {
+                leftest[y] = x as i32;
+            }
+            Space::Empty => {}
+        });
+    });
+}
+
+fn roll_rocks_south(world: &mut World) {
+    let mut lowest = std::iter::repeat(world.len() as i32)
+        .take(world[0].len())
+        .collect::<Vec<_>>();
+
+    (0..world.len()).rev().for_each(|y| {
+        (0..world[0].len()).for_each(|x| match world[y][x] {
+            Space::RoundRock => {
+                lowest[x] -= 1;
+                let final_y = lowest[x] as usize;
+                world[y][x] = Space::Empty;
+                world[final_y][x] = Space::RoundRock;
+            }
+            Space::CubeRock => {
+                lowest[x] = y as i32;
+            }
+            Space::Empty => {}
+        });
+    });
+}
+
+fn roll_rocks_east(world: &mut World) {
+    let mut rightest = std::iter::repeat(world[0].len() as i32)
+        .take(world.len())
+        .collect::<Vec<_>>();
+
+    (0..world[0].len()).rev().for_each(|x| {
+        (0..world.len()).for_each(|y| match world[y][x] {
+            Space::RoundRock => {
+                rightest[y] -= 1;
+                let final_x = rightest[y] as usize;
+                world[y][x] = Space::Empty;
+                world[y][final_x] = Space::RoundRock;
+            }
+            Space::CubeRock => {
+                rightest[y] = x as i32;
+            }
+            Space::Empty => {}
+        });
+    });
+}
+
+fn roll_one_cycle(world: &mut World) {
+    roll_rocks_north(world);
+    roll_rocks_west(world);
+    roll_rocks_south(world);
+    roll_rocks_east(world);
+}
+
+fn print_world(world: &World) -> String {
+    world.iter().fold(String::new(), |mut acc, row| {
+        row.iter().for_each(|space| {
+            acc.push(match space {
+                Space::RoundRock => 'O',
+                Space::CubeRock => '#',
+                Space::Empty => '.',
+            });
+        });
+        acc.push('\n');
+        acc
+    })
+}
+
 fn calculate_load(world: &World) -> usize {
     world
         .iter()
@@ -67,8 +149,34 @@ fn p1(input: &str) -> String {
 }
 
 fn p2(input: &str) -> String {
-    let _input = input.trim();
-    "".to_string()
+    let mut world = parse_input(input);
+
+    use std::collections::HashMap;
+    let mut seen: HashMap<String, usize> = HashMap::new();
+
+    const TOTAL_CYCLES: usize = 1_000_000_000;
+
+    let mut cycle: usize = 0;
+    let cycle_start: usize = loop {
+        cycle += 1;
+        roll_one_cycle(&mut world);
+
+        let current_world = print_world(&world);
+
+        if seen.contains_key(&current_world) {
+            break *seen.get(&current_world).unwrap();
+        } else {
+            seen.insert(current_world, cycle);
+        }
+    };
+
+    let rem = (TOTAL_CYCLES - cycle_start) % (cycle - cycle_start);
+
+    (0..rem).for_each(|_| {
+        roll_one_cycle(&mut world);
+    });
+
+    calculate_load(&world).to_string()
 }
 
 fn main() {
@@ -104,13 +212,71 @@ O.#..O.#.#
     }
 
     #[test]
-    fn test_p2_sample() {
-        assert_eq!(p2(SAMPLE_INPUT), "");
+    fn test_roll_cycles() {
+        let mut world = parse_input(SAMPLE_INPUT);
+
+        roll_one_cycle(&mut world);
+        assert_eq!(
+            print_world(&world).trim(),
+            r"
+.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....
+"
+            .trim()
+        );
+
+        roll_one_cycle(&mut world);
+        assert_eq!(
+            print_world(&world).trim(),
+            r"
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O
+"
+            .trim()
+        );
+
+        roll_one_cycle(&mut world);
+        assert_eq!(
+            print_world(&world).trim(),
+            r"
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O
+"
+            .trim()
+        );
     }
 
     #[test]
-    #[ignore = "not yet implemented"]
+    fn test_p2_sample() {
+        assert_eq!(p2(SAMPLE_INPUT), "64");
+    }
+
+    #[test]
     fn test_p2_actual() {
-        assert_eq!(p2(ACTUAL_INPUT), "");
+        assert_eq!(p2(ACTUAL_INPUT), "101010");
     }
 }
